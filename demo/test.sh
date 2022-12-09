3738cd30043ef4580e66586fac1bd39fcd05bc2f
Teemu Spoof <tspoof@c2r5p9.hive.fi>
2022-12-10 00:37:18 +0200

    updated demo

--- demo/test.sh
+++ demo/test.sh
@@ -2,4 +2,4 @@
 
 docker build -t malloc-failer ..
 docker run -dti --name malloc_failer -v $(dirname $(pwd)):/project/ malloc-failer
-docker exec -ti malloc_failer make -C ./demo
+docker exec -ti malloc_failer make test -C ./demo
